d35594e36a170bfd1ffb03c9d33a8e1d0249ecaf
Jenn Levy <jennbird91@aol.com>
2023-03-12 14:22:45 -0400

    make separate functions to clean up code

--- script.js
+++ script.js
@@ -11,21 +11,33 @@ function square(num) {
 
 //num is the input. the first and second function do the same thing but the second one is BETTER/EASIER
 
-let score = -1;
+let score = 100;
 const scoreElement = document.getElementById("score");
 const circle = document.getElementById("circle");  //this is a function that gets the element with the id circle
 
-
-
 function placeCircleRandomly() {
     circle.style.top = Math.random() * (window.innerHeight - 50) + "px";
-    circle.style.left = Math.random() * (window.innerWidth - 50) + "px";
-    score = score + 1;
+    circle.style.left = Math.random() * (window.innerWidth - 50) + "px";    
+}
+
+function handleCircleClick() {
+    console.log("I've been clicked!");
+    placeCircleRandomly();
+    increaseScore();
+    updateScore();
+}
+
+function increaseScore() {
+    score = score + 30;
     console.log("score " + score);
+}
+
+function updateScore() {
     scoreElement.innerText = score;
 }
 
-circle.addEventListener("click", placeCircleRandomly)
+circle.addEventListener("click", handleCircleClick);
 placeCircleRandomly();
+updateScore();
 
 
