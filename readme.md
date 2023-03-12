e4099b2404d25a1a95c782d8a31d28aee8a1753f
Jenn Levy <jennbird91@aol.com>
2023-03-12 13:46:04 -0400

    display changing points on screen

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 1. display your 100 points [ ]
 2. display circle [X]
-3.  add click listener [ ]
+3.  add click listener [X]
 4. click w/ in x amt of seconds, display appropriate points. Or no click then deduct 10 points [ ]
   a.click w/in 1 sec, get 30 points
   b. click w/in 2 sec get 20 points
   c. click w/in 3 sec, get 10 points
   d. change circle color once its non clickable
-  e. click greater than 3 sec, or no click, deduct 10 points
+  e. click greater than 3 sec, or no click, or click outside of circle, deduct 10 points
   f. at 3.5 seconds circle disappears and still deduct 10 points
   g. circle reappears somewhere else
   h. loop back to 2.
--- script.js
+++ script.js
@@ -11,13 +11,21 @@ function square(num) {
 
 //num is the input. the first and second function do the same thing but the second one is BETTER/EASIER
 
-
+let score = -1;
+const scoreElement = document.getElementById("score");
 const circle = document.getElementById("circle");  //this is a function that gets the element with the id circle
+
+
 
 function placeCircleRandomly() {
     circle.style.top = Math.random() * (window.innerHeight - 50) + "px";
     circle.style.left = Math.random() * (window.innerWidth - 50) + "px";
+    score = score + 1;
+    console.log("score " + score);
+    scoreElement.innerText = score;
 }
 
 circle.addEventListener("click", placeCircleRandomly)
 placeCircleRandomly();
+
+
